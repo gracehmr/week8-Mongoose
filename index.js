76c93c8c6cc04d563e5abbe6f8171591f881eb89
Grace Hamer <grace.hamer@outlook.com>
2021-10-14 14:44:57 +0100

    created update and delete functions

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { hideBin } = require("yargs/helpers");
 const argv = yargs(hideBin(process.argv)).argv;
 
 const mongoose = require("mongoose");
-const { string } = require("yargs");
 
 mongoose.connect('mongodb://localhost:27017/my-friends');
 
@@ -39,6 +38,36 @@ const findSome = async (name) => {
     process.exit();
 };
 
+const updateOne = async (name, newName) => {
+    const friend = await Friend.updateOne({name: name}, {name: newName});
+    console.log(friend);
+    process.exit();
+};
+
+const updateMany = async (name, newName) => {
+    const friend = await Friend.updateMany({name: name}, {name: newName});
+    console.log(friend);
+    process.exit();
+};
+
+const deleteOne = async (name) => {
+    const friend = await Friend.deleteOne({name: name});
+    console.log(friend);
+    process.exit();
+};
+
+const deleteMany = async (name) => {
+    const friend = await Friend.deleteMany({name: name});
+    console.log(friend);
+    process.exit();
+};
+
+const deleteAll = async () => {
+    const friend = await Friend.deleteMany();
+    console.log(friend);
+    process.exit();
+}
+
 const main = async (argv) => {
     try {
         if(argv.add) {
@@ -48,7 +77,17 @@ const main = async (argv) => {
         } else if (argv.findAll) {
             await findAll();
         } else if (argv.findSome) {
-            await findSome(argv.name)
+            await findSome(argv.name);
+        } else if (argv.updateOne) {
+            await updateOne (argv.name, argv.newName);
+        } else if (argv.updateMany) {
+            await updateMany (argv.name, argv.newName);
+        } else if (argv.deleteOne) {
+            await deleteOne (argv.name);
+        } else if (argv.deleteMany) {
+            await deleteMany (argv.name);
+        } else if (argv.deleteAll) {
+            await deleteAll();
         }
     } catch (error) {
         console.log(error)
